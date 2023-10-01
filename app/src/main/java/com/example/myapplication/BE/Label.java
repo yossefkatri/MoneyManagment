f4bb80b807511560f65e5ef44f3a257a7ceb2d60
yossef katri <yossefkatri@gmail.com>
2023-10-01 23:18:20 +0300

    middle of goal - finshed with adding and now working on update and remove

--- app/src/main/java/com/example/myapplication/BE/Label.java
+++ app/src/main/java/com/example/myapplication/BE/Label.java
@@ -2,6 +2,7 @@ package com.example.myapplication.BE;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Label {
     String name;
@@ -30,5 +31,18 @@ public class Label {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Label label = (Label) o;
+        return name.equals(label.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- app/src/main/java/com/example/myapplication/UI/activities/Goal.java
+++ app/src/main/java/com/example/myapplication/UI/activities/Goal.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,10 +16,14 @@ import com.example.myapplication.BE.Limit;
 import com.example.myapplication.R;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Goal extends AppCompatActivity implements View.OnClickListener {
+
+    List<Limit> tempLimits = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,23 +47,65 @@ public class Goal extends AppCompatActivity implements View.OnClickListener {
         List<Label> labels = createLabelsFromInput(String.valueOf(labelsText.getText()));
         try {
             limit.setLimit(Integer.parseInt(String.valueOf(limitText.getText())));
+            labels.sort(Comparator.comparing(Label::getName));
             limit.setLabels(labels);
         } catch (NumberFormatException e) {
             Log.i(this.getClass().toString(), "limit is wrong input ,the input should be number");
             return;
         }
-        addLimitToView(limit);
+        int index = getIndex(labels);
+        if (index < 0) {
+            addLimitToView(limit);
+            tempLimits.add(limit);
+        } else {
+            tempLimits.get(index).setLimit(limit.getLimit());
+            updateLimitToView(limit);
+        }
+        Toast.makeText(this,"tempLimits size: "+tempLimits.size(),Toast.LENGTH_LONG).show();
+    }
+
+    private void updateLimitToView(Limit limit) {
+        //TODO implement the method
+    }
+
+    /**
+     * @param labels labels input from the user
+     * @return the index in the list which this limit is located, otherwise -1
+     */
+    private int getIndex(List<Label> labels) {
+        for (int i = 0; i < tempLimits.size(); ++i) {
+            if (tempLimits.get(i).getLabels().equals(labels)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     private void addLimitToView(Limit limit) {
-        LinearLayout goals = (LinearLayout) findViewById(R.id.goals);
+        LinearLayout goals = findViewById(R.id.goals);
 
-        TextView labels;
-        TextView limit;
-        LinearLayout limitLayer = new LinearLayout(this);
-        limitLayer.setOrientation(LinearLayout.HORIZONTAL);
+        TextView limitView = new TextView(this);
+        limitView.setTextSize(15);
+        limitView.setText(createLimitString(limit));
+        //TextView limit;
+        //LinearLayout limitLayer = new LinearLayout(this);
+        //limitLayer.setOrientation(LinearLayout.HORIZONTAL);
 
-        goals.add
+        goals.addView(limitView);
+        goals.invalidate();
+    }
+
+    private String createLimitString(Limit limit) {
+        List<Label> labels = limit.getLabels();
+        StringBuilder res = new StringBuilder();
+        res.append("label: ");
+        for (Label label : labels) {
+            res.append(label.getName());
+            res.append("+");
+        }
+        res.deleteCharAt(res.length() - 1);
+        res.append(" limit: " + limit.getLimit());
+        return res.toString();
     }
 
     private List<Label> createLabelsFromInput(String labelsRaw) {
